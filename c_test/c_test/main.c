0492d680fca230d510b009b2fe0c67d9b3006c3f
bennyrhys <bennyrhys@163.com>
2019-06-30 19:05:43 +0800

    1.3复杂的表达式计算

--- c_test/c_test/main.c
+++ c_test/c_test/main.c
@@ -7,9 +7,10 @@
 //
 
 #include <stdio.h>
+#include <math.h>//引入数学函数库
 
-int main(int argc, const char * argv[]) {
-    // 除法，保留一位小数
-    printf("%.1f", 7 / 5.0);
+int main() {
+    // 1.3复杂的表达式计算
+    printf("%.8f\n", 1 + 2 * sqrt(3) / (2 - 1.5));
     return 0;
 }
