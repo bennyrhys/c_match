78f66a60cf150fb72825d1556bde0f7cf3784ba1
bennyrhys <bennyrhys@163.com>
2019-06-30 17:28:17 +0800

    hello world

---
+++ c_test/c_test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c_test
+//
+//  Created by bennyrhys on 2019/6/30.
+//  Copyright © 2019 bennyrhys. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
