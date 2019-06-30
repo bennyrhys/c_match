aa07ceeb4c164eff6a57f5f228a54944a0f391ad
bennyrhys <bennyrhys@163.com>
2019-06-30 19:14:27 +0800

    1.2变量及其输入:1-4a+b问题

--- c_test/c_test/main.c
+++ c_test/c_test/main.c
@@ -6,11 +6,13 @@
 //  Copyright © 2019 bennyrhys. All rights reserved.
 //
 
-#include <stdio.h>
-#include <math.h>//引入数学函数库
+#include <stdio.h>//控制输入输出io流
 
 int main() {
-    // 1.3复杂的表达式计算
-    printf("%.8f\n", 1 + 2 * sqrt(3) / (2 - 1.5));
+    //1.2变量及其输入
+    // 1-4a+b问题
+    int a, b;
+    scanf("%d%d", &a, &b);//注意此时“”内不要写多余内容容易引发报错
+    printf("%d\n", a + b);
     return 0;
 }
