b1fb51d7d560989935a76e324c0d2606b1cfaca1
bennyrhys <bennyrhys@163.com>
2019-07-05 10:55:04 +0800

    1.3顺序结构设计1-6三位数反转

--- c_test/c_test/main.c
+++ c_test/c_test/main.c
@@ -7,17 +7,12 @@
 //
 
 #include <stdio.h>//控制输入输出io流
-#include <stdio.h>//引入数学函数
 
 int main() {
-    //1.2变量及其输入
-    // 1-5计算圆柱表面积，输入半径r，高h，输出表面积，保留三位小数
-    const double pi = acos(-1.0);//const使变量不可变。acos(-1.0)值为派。
-    double s , s1, s2, r, h;
-    scanf("%lf%lf", &r,&h);//输入数据是float时用%f,当输入数据是double时用%lf，输出最好都用%f
-    s1 = pi *r *r;
-    s2 = 2 *pi *r;
-    s = s1 *2 + s2 *h;
-    printf("半径：%.3f,高:%.3f,表面积：%.3f\n", r, h, s);
+    //1.3顺序结构设计
+    // 1-6三位数反转
+    int n;
+    scanf("%d", &n);
+    printf("%d%d%d\n", n%10, n/10%10, n/100);
     return 0;
 }
