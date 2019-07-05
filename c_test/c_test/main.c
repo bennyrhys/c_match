1d7a419a429c52087f799504a3d97c7308b5ac17
bennyrhys <bennyrhys@163.com>
2019-07-05 12:08:05 +0800

    1.3分支结构的程序设计1-11鸡兔同笼

--- c_test/c_test/main.c
+++ c_test/c_test/main.c
@@ -1,7 +1,7 @@
 //
 //  main.c
 //  c_test
-//
+//  Author：bennyrhys@163.com
 //  Created by bennyrhys on 2019/7
 //  Copyright © 2019 bennyrhys. All rights reserved.
 //
@@ -9,10 +9,18 @@
 #include <stdio.h>//控制输入输出io流
 
 int main() {
-    //1.3顺序结构设计
-    //1-10交换变量-适合比赛的黑盒测试
-    int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d %d\n",b, a);
+    //1.3d分支结构的程序设计
+    //1-11鸡兔同笼
+    //输入 总只数：n总腿数：m
+    //输出 如果没有结果返回无解，鸡数 兔数
+    int m, n, a, b;//a,b分别为鸡兔个数
+    scanf("%d%d", &n, &m);
+    a = (4 * n- m )/ 2;
+    b = n - a;
+    if(m % 2 == 1 || a < 0 || b < 0){
+        printf("无解");
+    }else{
+        printf("%d %d",a ,b);
+    }
     return 0;
 }
