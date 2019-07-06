4c63ad38b802b655c4722ca67c74afd80d20bee3
bennyrhys <bennyrhys@163.com>
2019-07-06 11:34:59 +0800

    2-4while 3n+1-longlong处理溢出

--- c_test/c_test/main.c
+++ c_test/c_test/main.c
@@ -10,12 +10,13 @@
 
 int main() {
     //2章循环结构程序设计
-    //2-4while 3n+1
+    //2-4while 3n+1-longlong处理溢出
     //输入 大于1任意自然数
     //输出 变换次数
     //注意！奇数3n+1 偶数/2
-    int n, count = 0;
-    scanf("%d", &n);
+    int n2, count = 0;
+    scanf("%d", &n2);
+    long long n = n2;//使用此方法暂替long，之后用long重新处理 范围溢出 %lld……
     while (n > 1) {
         if(n % 2 == 1) n = 3 * n + 1;
         else n /= 2;
