d5b1b97a399f7202af964cec096bfcb9905d6b30
bennyrhys <bennyrhys@163.com>
2019-07-06 10:41:52 +0800

    2-3aabb-枚举平方根

--- c_test/c_test/main.c
+++ c_test/c_test/main.c
@@ -10,17 +10,18 @@
 
 int main() {
     //2章循环结构程序设计
-    //2-2aabb-+0.5
+    //2-3aabb-枚举平方根
     //输入
     //输出 所有完全平方数形式的aabb
-    //注意！完全平方数：先开方，再平方比较。aabb = 1100 + 11
-    for (int i = 1; i <= 9; i++) {
-        for (int j = 0; j <= 9; j++) {
-            int n = 1100 * i + 11 * j;//这里使用，这里定义，尽力缩小范围
-            int m = floor(sqrt(n) + 0.5);//避免浮点运算误差+0.5
-            if(m * m == n)
-                printf("%d\n", n);
-        }
+    //注意！完全平方数 列举所有组合数字。aabb = 1100 + 11
+    for (int i = 1;; i++) {
+        int n = i * i;
+        if(n < 1000) continue;
+        if(n > 9999) break;
+        int hi = n / 100;
+        int lo = n % 100;
+        if (hi % 10 == hi / 10 && lo % 10 == lo /10)
+            printf("%d", n);
     }
     return 0;
 }
